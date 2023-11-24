b8bd26e5c13d20b8cae8949fb2e8181965edb2b6
AmmelyStar <113164996+AmmelyStar@users.noreply.github.com>
2023-11-24 15:51:55 +0100

    second task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import user from 'components/Profile/user.json';
 import { Profile } from './Profile/Profile';
+import data from 'components/Statistics/data.json';
+import {Statistics} from './Statistics/Statistics.jsx';
 
 export const App = () => {
   return (
@@ -11,6 +13,11 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
+      <Statistics title="Upload stats" stats={data} />
+      
     </>
   );
 };
+
+
+
